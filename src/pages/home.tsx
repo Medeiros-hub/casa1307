ecf084a0db96a196a2837c51ee78624d1d11ada2
Pedro Henrique Medeiros <medeiroshenriqueramalhopedro@gmail.com>
2024-07-28 19:22:46 -0300

    feat: description section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,11 @@
+import { BadgePlus, Crown, LayoutDashboard, Star } from 'lucide-react'
+
+import { DescriptionCard } from '../components/description-card'
 import { Topbar } from '../components/topbar'
 
 export function Home() {
   return (
-    <div className="w-full space-y-16">
+    <div className="w-full space-y-20">
       <Topbar />
 
       <section className="mx-16 rounded-2xl bg-bg-sm bg-cover bg-no-repeat md:bg-bg-large md:bg-center">
@@ -32,6 +35,45 @@ export function Home() {
           </button>
         </div>
       </section>
+
+      <section className="mx-16 flex justify-between gap-28">
+        <div className="max-w-[41.5rem] space-y-14">
+          <h1 className="text-3xl md:text-5xl">
+            Somos um lar acolhedor para o sucesso da sua empresa!
+          </h1>
+          <div className="flex flex-wrap gap-10">
+            <DescriptionCard
+              Icon={Star}
+              title="Delicada"
+              description="Com um toque sutil e elegante, oferece uma experiência planejada com atenção as necessidades dos clientes."
+            />
+            <DescriptionCard
+              Icon={BadgePlus}
+              title="Moderna"
+              description="É sinônimo de modernidade, oferecendo um espaço que combina inovação e estilo contemporâneo."
+            />
+            <DescriptionCard
+              Icon={LayoutDashboard}
+              title="Digital"
+              description="A Casa1307 está sempre conectada, proporcionando uma experiência digital avançada e acessível."
+            />
+            <DescriptionCard
+              Icon={Crown}
+              title="Nobre"
+              description="Nos destacmos pela sua excelência e sofisticação, proporcionando um ambiente de prestígio para todos os seus clientes."
+            />
+          </div>
+        </div>
+        <div className="hidden max-w-[31.25rem] items-center justify-center rounded-2xl bg-bg-sm bg-cover bg-no-repeat px-20 lg:flex">
+          <img
+            src="/logo-white.svg"
+            alt="logo Casa1307"
+            className="size-[20.4375rem]"
+          />
+        </div>
+      </section>
+
+      <section className="mx-16 h-[40.625rem] self-center rounded-2xl bg-bg-sm bg-cover bg-no-repeat sm:bg-bg-xl"></section>
     </div>
   )
 }
